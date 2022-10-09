8e1c3b152f3738771e55f1c12a0f8cbfd90243b1
Liridona <91525128+barlaek@users.noreply.github.com>
2022-10-09 20:28:18 +0200

    register user

--- js/components/requests/registration.mjs
+++ js/components/requests/registration.mjs
@@ -4,50 +4,38 @@ const API_BASE_URL = 'https://nf-api.onrender.com';
 
 //Register endpoint: /api/v1/social/auth/register
 
-// Setting the input values for the newUser object
-
 const form = document.getElementById('regForm');
-const newName = document.getElementById('newName').value;
-const newEmail = document.getElementById('newEmail').value;
-const newPw = document.getElementById('newPw').value;
 
-// Test object
-// const userData = JSON.stringify({
-//     name: "test_bombadil",
-//     email: "bombadil_test@noroff.no",
-//     password: "12345678"
-// })
-
-// Registration function
-
-async function registerUser(event) {
+form.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    const testObject = JSON.stringify({
-        name: newName,
-        email: newEmail,
-        password: newPw
-    });
+    const form = event.target;
 
-    const postOptions = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: testObject,
-    }   
+    const newUser = {
+        name: form.newName.value,
+        email: form.newEmail.value,
+        password: form.newPw.value,
+    }
 
-    try {
+    console.log(newUser);
+
+    async function registerUser(newUser) {
+        const postOptions = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newUser)
+        };
+
         const response = await fetch(`${API_BASE_URL}/api/v1/social/auth/register`, postOptions);
         console.log(response);
         const json = await response.json();
         console.log(json);
-        if(json) {
-            window.location.href="/index.html";
-        }
-    } catch(error) {
-        console.log(error);
-    }
-}
 
-form.addEventListener('submit', registerUser);
+        if(json.status === 200) {
+            window.location.href='/index.html';
+        }
+    }
+    registerUser(newUser);
+})
