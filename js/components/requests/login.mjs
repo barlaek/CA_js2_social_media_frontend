ec799633255e5632e516be5e8da4b8d4521bbea4
Liridona <91525128+barlaek@users.noreply.github.com>
2022-10-09 21:10:32 +0200

    commets and cleanup

--- js/components/requests/login.mjs
+++ js/components/requests/login.mjs
@@ -39,39 +39,7 @@ form.addEventListener('submit', (event) => {
     loginUser(user);
 })
 
-// const userLogin = document.getElementById('userLogin');
-// const userEmail = document.getElementById('userEmail').value;
-// const userPw = document.getElementById('userPw').value;
-
-// async function loginUser(event) {
-//     event.preventDefault();
-
-//     const postData = JSON.stringify({
-//         email: userEmail,
-//         email: userPw
-//     });
-
-//     const postOptions = {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json',
-//         },
-//         body: postData,
-//     };
-
-//     try {
-//         const response = await fetch(`${API_BASE_URL}/api/v1/social/auth/login`, postOptions);
-//         console.log(response);
-//         const json = await response.json();
-//         console.log(json);
-
-//         const accessToken = json.accessToken;
-//         localStorage.setItem('accessToken', accessToken);
-//         console.log(accessToken);
-
-//     } catch(error) {
-//         console.log(error);
-//     }
-// }
-
-// userLogin.addEventListener('submit', loginUser);
+// Test user:
+// test_bombadil
+// bombadil_test@noroff.no
+// 12345678
