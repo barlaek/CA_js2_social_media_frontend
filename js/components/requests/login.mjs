2a9c2fd9a7e23b828d34353f13ddfbfad6521be0
Liridona <91525128+barlaek@users.noreply.github.com>
2022-10-09 20:40:30 +0200

    login api

--- js/components/requests/login.mjs
+++ js/components/requests/login.mjs
@@ -2,39 +2,70 @@ const API_BASE_URL = 'https://nf-api.onrender.com';
 
 // Endpoint: /api/v1/social/auth/login
 
-const userLogin = document.getElementById('userLogin');
-const userEmail = document.getElementById('userEmail').value;
-const userPw = document.getElementById('userPw').value;
+const form = document.getElementById('logForm');
 
-async function loginUser(event) {
+form.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    const postData = JSON.stringify({
-        email: userEmail,
-        email: userPw
-    });
+    const form = event.target;
 
-    const postOptions = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: postData,
-    };
-
-    try {
-        const response = await fetch(`${API_BASE_URL}/api/v1/social/auth/login`, postOptions);
-        console.log(response);
-        const json = await response.json();
-        console.log(json);
-
-        const accessToken = json.accessToken;
-        localStorage.setItem('accessToken', accessToken);
-        console.log(accessToken);
-
-    } catch(error) {
-        console.log(error);
+    const user = {
+        email: form.userEmail.target,
+        password: form.userPw.target,
     }
-}
 
-userLogin.addEventListener('submit', loginUser);
+    console.log(user);
+
+    async function loginUser(user) {
+        const loginOptions = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(user)
+        };
+
+        const response = await fetch(`${API_BASE_URL}/api/v1/social/auth/login`, loginOptions);
+        console.log(response);
+        const json = response.json();
+        console.log(json);
+    }
+    loginUser(user);
+})
+
+// const userLogin = document.getElementById('userLogin');
+// const userEmail = document.getElementById('userEmail').value;
+// const userPw = document.getElementById('userPw').value;
+
+// async function loginUser(event) {
+//     event.preventDefault();
+
+//     const postData = JSON.stringify({
+//         email: userEmail,
+//         email: userPw
+//     });
+
+//     const postOptions = {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//         body: postData,
+//     };
+
+//     try {
+//         const response = await fetch(`${API_BASE_URL}/api/v1/social/auth/login`, postOptions);
+//         console.log(response);
+//         const json = await response.json();
+//         console.log(json);
+
+//         const accessToken = json.accessToken;
+//         localStorage.setItem('accessToken', accessToken);
+//         console.log(accessToken);
+
+//     } catch(error) {
+//         console.log(error);
+//     }
+// }
+
+// userLogin.addEventListener('submit', loginUser);
