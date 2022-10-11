a11662feaaca721d7522cd882a9f7e8917819ed2
Liridona <91525128+barlaek@users.noreply.github.com>
2022-10-11 17:59:15 +0200

    display feed

--- js/components/requests/posts.mjs
+++ js/components/requests/posts.mjs
@@ -21,9 +21,29 @@ async function getPosts(url) {
         console.log(response);
         const json = await response.json();
         console.log(json);
+        if(response.ok) {
+            viewContent(json);
+        }
     } catch(error) {
         console.log(error)
     }
 }
 
 getPosts(`${API_BASE_URL}/api/v1/social/posts/`);
+
+const postsContainer = document.getElementById('feedContainer');
+
+function viewContent(posts) {
+    for(let i = 0; i < i.length; i++) {
+        postsContainer.innerHTML +=
+        `<div class="card mb-4">
+            <div class="card-body">
+                <h5 class="card-title">${posts[i].title}</h5>
+                <p class="card-text"><p class="card-text">${posts[i].body}</p></p>
+                <p class="card-text"><small class="text-muted">${posts[i].created}</small></p>
+            </div>
+            <img src="..." class="card-img-bottom" alt="...">
+        </div>
+        `
+    }
+}
