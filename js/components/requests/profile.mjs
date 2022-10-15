c9a6a7a3650932779d974a927669e62fb1a50cf2
Liridona <91525128+barlaek@users.noreply.github.com>
2022-10-15 15:49:17 +0200

    attempting to get single profile

--- js/components/requests/profile.mjs
+++ js/components/requests/profile.mjs
@@ -5,31 +5,6 @@ import { profilesUrl } from "./api.mjs";
 // const params = new URLSearchParams(queryString);
 // const name = params.get('name');
 
-// console.log(name);
-
-// async function getProfile() {
-//     try{
-//         const token = localStorage.getItem('accessToken')
-        
-//         const profileOptions = {
-//             method: 'GET',
-//             headers: {
-//                 'Content-Type': 'application/json',
-//                 Authorization: `Bearer ${token}`,
-//             },
-//         };
-
-//         const response = await fetch(`${API_BASE_URL}/api/v1/social/profiles/${name}?_posts=true&_following=true&_followers=true`, profileOptions)
-//         console.log(response);
-//         const json = await response.json();
-//         console.log(json);
-//     } catch(error) {
-//         console.log(error);
-//     }
-// }
-
-// getProfile();
-
 async function getAllProfiles() {
     try {
         const token = localStorage.getItem('accessToken')
@@ -52,3 +27,28 @@ async function getAllProfiles() {
 }
 
 getAllProfiles();
+
+async function getSingleProfile(name) {
+    try{
+
+        const singleProfile = `${profilesUrl}${name}`;
+
+        const token = localStorage.getItem('accessToken')
+
+        const options = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`,
+            },
+        };
+
+        const response = await fetch(singleProfile, options);
+        console.log(response)
+        const json = await response.json();
+        console.log(json)
+
+    } catch(error) {
+        console.log(error)
+    }
+}
