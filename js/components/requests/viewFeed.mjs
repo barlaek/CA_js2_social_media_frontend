672d69fa191c8d64f5fb9d7420074c5359fce919
Liridona <91525128+barlaek@users.noreply.github.com>
2022-10-16 17:43:43 +0200

    still trying to add search functionality

--- js/components/requests/viewFeed.mjs
+++ js/components/requests/viewFeed.mjs
@@ -1,7 +1,7 @@
 
 import { postsURL } from "./api.mjs";
 
-const viewFeedEndPoint = `${postsURL}/?_author=true&_comments=true&_reactions=true`;
+// const viewFeedEndPoint = `${postsURL}/?_author=true&_comments=true&_reactions=true`;
 const token = localStorage.getItem('accessToken');
 const getOptions = {
     method: 'GET',
@@ -11,21 +11,34 @@ const getOptions = {
     },
 };
 export const postsContainer = document.getElementById('feedContainer');
+const search = document.getElementById('search');
+
+let posts = [];
+
+search.addEventListener('keyup', (event) => {
+    const searchString = event.target.value.toLowerCase();
+    const filteredPosts = posts.filter(post => {
+        return (post.title.toLowerCase().includes(searchString) || post.owner.toLowerCase().includes(search))
+    });
+    viewContent(filteredPosts);
+})
 
 async function getPosts(url) {
 
     try{
         const response = await fetch(url, getOptions)
-        const json = await response.json();
+        posts = await response.json();
         if(response.ok) {
-            viewContent(json);
+            viewContent(posts);
         }
+
+        console.log(response);
     } catch(error) {
         console.log(error)
     }
 }
 
-getPosts(viewFeedEndPoint);
+getPosts(postsURL);
 
 export function viewContent(posts) {
     postsContainer.innerHTML += '';
@@ -37,7 +50,7 @@ export function viewContent(posts) {
             postsContainer.innerHTML +=
             `<a class="card mb-4 text-decoration-none text-reset" href="singlePost.html?id=${post.id}">
                 <div class="card-body">
-                    <h5 class="card-title">${post.author.name}</h5>
+                    <h5 class="card-title">${post.title}</h5>
                     <p class="card-text"><p class="card-text">${post.body}</p></p>
                     <p class="card-text"><small class="text-muted">${date}</small></p>
                 </div>
@@ -46,78 +59,3 @@ export function viewContent(posts) {
         })
     }
 }
-
-const search = document.getElementById('search');
-const searchMenu = document.getElementById('searchMenu');
-const searchCard = document.getElementById('searchCard');
-
-async function searchPosts(url) {
-    try {
-        const response = await fetch(url, getOptions)
-        const json = await response.json();
-        console.log(json)
-
-        search.addEventListener('input', (event) => {
-            const value = event.target.value.toLowerCase();
-            if(value) {
-                viewSearch(json);
-            }
-        })
-    } catch(error) {
-        console.log(error)
-    }
-}
-
-searchPosts(postsURL);
-
-function viewSearch(posts) {
-    searchMenu.innerHTML += '';
-    posts.map((post) => {
-        searchMenu.innerHTML +=
-            `<li id="searchCard">
-                <a id="searchBody">${post.id}</a>
-            </li>
-            `
-    })
-}
-
-
-// search.addEventListener('input', (event) => {
-//     const value = event.target.value.toLowerCase();
-//     posts.forEach(post => {
-//         const isVisible = post.id.toLowerCase.includes(value);
-//         post.element.classList.toggle('hide', !isVisible)
-//     })
-// })
-
-// fetch(postsURL, getOptions)
-//     .then(response => response.json())
-//     .then(data => {
-//         posts = data.map(post => {
-//             const card = searchCard.content.cloneNode(true).children[0];
-//             const body = card.getElementById('searchBody');
-//             body.textContent = post.id;
-//             searchMenu.append(card);
-//             return { id: post.id, element: card};
-//         })
-//     })
-
-// search.addEventListener('input', async (event) => {
-//     event.preventDefault();
-//     const value = event.target.value;
-//     if(value) {
-//         const response = await fetch(postsURL, getOptions)
-//         const filteredResponse = response.filter((item) => {
-//             if(
-//                 item.title.toLowerCase().includes(value.toLowerCase()) ||
-//                 item.body.toLowerCase().includes(value.toLowerCase()) ||
-//                 item.id.toLowerCase().includes(value.toLowerCase()) ||
-//                 item.owner.toLowerCase().includes(value.toLowerCase())
-//                 ) {
-//                     return item;
-//                 }
-
-//         });
-//         console.log(filteredResponse);
-//     }
-// })
