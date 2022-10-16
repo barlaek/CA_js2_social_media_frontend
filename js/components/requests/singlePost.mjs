7ebe61cb1ff6f9dd8720a2df6da875269f702ec1
Liridona <91525128+barlaek@users.noreply.github.com>
2022-10-16 12:04:52 +0200

    update post

--- js/components/requests/singlePost.mjs
+++ js/components/requests/singlePost.mjs
@@ -1,4 +1,5 @@
 import { postsURL } from './api.mjs';
+import { token, profileOptions } from '../utilities/localStorage.mjs';
 
 const singPost = document.getElementById('singPost');
 
@@ -13,23 +14,15 @@ console.log(url);
 
 async function getSinglePost() {
     try {
-        const token = localStorage.getItem('accessToken');
 
-        const options = {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/js',
-                Authorization: `Bearer ${token}`,
-            },
-        };
-
-        const response = await fetch(url, options);
+        const response = await fetch(url, profileOptions);
         console.log(response);
         const json = await response.json();
         console.log(json);
 
         if(response.ok) {
             displayPost(json);
+            // updatePost(json);
         }
     } catch(error) {
         console.log(error)
@@ -38,14 +31,87 @@ async function getSinglePost() {
 
 getSinglePost();
 
+// async function updatePost(data) {
+//     try {
+
+//         const upDateBody = {
+//             title: data.title,
+//             body: data.body,
+//         };
+
+//         const updateOptions = {
+//             method: 'PUT',
+//             header: {
+//                 'Content-Type': 'application/json',
+//                 Authorization: `Bearer ${token},`
+//             },
+//             body: JSON.stringify(upDateBody),
+//         };
+
+//         const response = await fetch(url, updateOptions);
+//         console.log(response);
+//         const json = await response.json();
+//         console.log(json);
+
+//     } catch(error) {
+//         console.log(error)
+//     }
+
+// }
+
+
 function displayPost(post) {
     singPost.innerHTML +=
-    `<div class="card mb-4">
+    `<div class="card mb-4" id="updateForm">
         <div class="card-body">
             <h5 class="card-title">${post.title}</h5>
             <p class="card-text"><p class="card-text">${post.body}</p></p>
             <p class="card-text"><small class="text-muted">${post.created}</small></p>
+            <input id="update" class="form-control col text-muted" type="text" placeholder="Update post">
+            <button type="submit" class="btn btn-warning col-2" id="updatePost">Update</button>
         </div>
     </div>
     `
+}
+
+const form = document.getElementById('singPost');
+
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const newUpdate = {
+        title: '',
+        body: form.update.value,
+    };
+
+    if(updatePost(newUpdate)){
+        // setTimeout(() => {
+        //     window.location.reload();
+        // }, 2000);
+    };
+
+    form.reset();
+});
+
+async function updatePost(data) {
+    try {
+        
+        const updateOptions = {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json; charset=UTF-8',
+                Authorization: `Bearer ${token}`,
+            },
+            body: JSON.stringify(data),
+        };
+
+        const response = await fetch(url, updateOptions);
+        console.log(response);
+        const json = await response.json();
+        console.log(json);
+
+    } catch(error) {
+        console.log(error)
+    }
 }
